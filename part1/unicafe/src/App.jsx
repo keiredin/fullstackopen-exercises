763045483b404ab4f02793c445adcda0974ca2fe
keiredin <se.keiredin.aman@gmail.com>
2026-09-24 17:14:30 +0300

    refactor the components and also display the statistics using table

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,30 +5,55 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const total = good + neutral + bad;
-  const average = (good - bad) / total;
-  const positive = (good / total) * 100;
 
   return (
     <>
       <div>
         <h1>give feedback</h1>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
+        <Button onClick={() => setGood(good + 1)} text="good" />
+        <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button onClick={() => setBad(bad + 1)} text="bad" />
       </div>
 
-      <div>
-        <h1>statistics</h1>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {total}</p>
-        <p>average {average}</p>
-        <p>positive {positive} %</p>
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   );
 };
 
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positive = (good / total) * 100;
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      {total === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={total} />
+            <StatisticLine text="average" value={average.toFixed(2)} />
+            <StatisticLine text="positive" value={`${positive.toFixed(2)}%`} />
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
 export default App;
